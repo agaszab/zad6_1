b3fbfdbfa9f1aa54934d602960cfe550c25c0102
agaszab <gatito@poczta.pl>
2018-01-27 18:56:31 +0100

    jedna tablica, jeden for

--- src/Program.java
+++ src/Program.java
@@ -4,19 +4,18 @@ import java.util.Random;
 public class Program {
     public static void main(String[] args) {
 
-        int[] tab=new int[10];
-        int[] tabRevers=new int[10];
-        int j=9;
+        int[] tab=new int[20];
+        int j=0;
         Random rand=new Random();
 
         for (int i = 0; i < tab.length; i++) {
-        tab[i]=rand.nextInt(10);
-        tabRevers[j]=tab[i];
-        j--;
+        if (i<tab.length/2)  tab[i]=rand.nextInt(10);
+        j=((tab.length)-1)-i;
+        tab[j]=tab[i];
+        System.out.print(tab[i]+" ");
         }
 
-        System.out.print(Arrays.toString(tab));
-        System.out.print(Arrays.toString(tabRevers));
+
 
 
 
